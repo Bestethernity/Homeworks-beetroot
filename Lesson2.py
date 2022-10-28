2bacd2615466f3c770663e98cb2c495c827bc644
Bestethernity <zamkovayasvetlana@gmail.com>
2022-10-28 15:15:13 +0100

    forth try

---
+++ Lesson2.py
@@ -0,0 +1,18 @@
+#Task 2
+#Create a python program named "task2", and use the built-in function 'print' in it several times.
+#Try to pass "sep", "end" params and pass several parameters separated by commas.
+#Also, provide a comment text above each print statement, mentioned above,
+#with the expected output after execution of the particular print statement.
+print("Hello world")
+print("hello", "world", sep='---',end='!') #two words separated with --- and with the ! symbol at the end
+print(1,2,3,4,5, sep='-')
+#using \n to separate the lines
+print("hello", "world", sep='---',end='!\n')
+print(1,2,3,4,5, sep='-')
+#Task 3
+#Write a program, which has two print statements to print the following text
+#(capital letters "O" and "H", made from "#" symbols):
+#Pay attention that usage of spaces is forbidden, as well as creating the whole result
+#text string using """ """, use '\n' and '\t' symbols instead.
+
+print('#########\n','#\t\t#\n','#\t\t#\n','#\t\t#\n','#########\n\n','#\t\t#\n','#\t\t#\n','#########\n','#\t\t#\n','#\t\t#\n')
